5c68699142368705c73ae9e27a2ef5c7e18948b2
Gaurav Agarwal <gauravag@thoughtworks.com>
2012-11-30 15:46:00 +0530

    Initial Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlanUrTrip::Application.config.secret_token = '2dcd4f458f9b8d847e9a29489a49aa7add5ff89d3b090bfa101522ca7408f48dbfe113c6fb9a31a28c2394463aa2d18dd59017d591b2f73509c6128e2f21474c'
